576840d270269f18d05541b12652dab8ee854b2f
LaptopKing <csordaspeter2002@gmail.com>
2025-05-17 19:30:28 +0200

    feat: added snacks.nvim from folke to enhance my workflow easier
    NOTE: Also removed the kickstart provided indent_line plugin and now
    using snacks.nvim's indent plugin

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,43 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    input = { enabled = true },
+    notify = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    words = { enabled = true },
+    indent = {
+      enabled = true,
+      animate = { enabled = false },
+      scope = {
+        enabled = true, -- enable highlighting the current scope
+        priority = 200,
+        char = '│',
+        underline = false, -- underline the start of the scope
+        only_current = false, -- only show scope in the current window
+        hl = 'SnacksIndent1',
+      },
+    },
+    dim = { enabled = true, animate = { enabled = false } },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VeryLazy',
+      callback = function()
+        -- Set dimming automatically at startup
+        Snacks.dim()
+
+        -- Create some toggle mappings
+        Snacks.toggle.indent():map '<leader>ug'
+        Snacks.toggle.dim():map '<leader>uD'
+      end,
+    })
+  end,
+}
